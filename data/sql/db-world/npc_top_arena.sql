b8e3e8cfb4d15a7896634722f17f8834186d309a
GeraldFV <61763884+GeraldFV@users.noreply.github.com>
2024-09-11 13:49:30 -0500

    Update npc_top_arena.sql
    modelid1 was not loaded
    
    Model in creature_template_model

--- data/sql/db-world/npc_top_arena.sql
+++ data/sql/db-world/npc_top_arena.sql
@@ -3,7 +3,7 @@
 -- --------------------------------------------------------------------------------------
 SET
 @Entry 		:= 55333,
-@Model 		:= 31833, -- Blood Elf
+@Model 	    := 31833, -- Blood Elf
 @Name 		:= "Destroyer",
 @Title 		:= "Top Arena Ranking",
 @Icon 		:= "LootAll",
@@ -20,10 +20,16 @@ SET
 @AIName		:= "SmartAI",
 @Script 	:= "arenatop";
 
--- NPC
+-- NPC in creature_template
 DELETE FROM creature_template WHERE entry = @Entry;
-INSERT INTO creature_template (`entry`, `modelid1`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `AiName`, `ScriptName`) VALUES
-(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, 1, 1.14286, @Scale, @Rank, 1, 2, @Type, @TypeFlags, 1, @FlagsExtra, @AIName, @Script);
+INSERT INTO creature_template (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) 
+VALUES (@Entry, 0, 0, 0, 0, 0, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, 1, @Faction, @NPCFlag, 1, 1.14286, 1, 1, 20, @Scale, @Rank, 0, 2.4, 2000, 2000, 1, 1, 1, 2, 2048, 0, 0, 0, 0, 0, 0, @Type, @TypeFlags, 0, 0, 0, 0, 0, 0, 0, @AIName, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, @FlagsExtra, @Script, 12340);
 
+-- Text in npc_text
 DELETE FROM `npc_text` WHERE `ID`=@Entry;
 INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES (@Entry, 'Hey there, the name\'s Skinny. You feelin\' lucky?');
+
+-- Model in creature_template_model
+DELETE FROM `creature_template_model` WHERE `CreatureID`=@Entry;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) 
+VALUES (@Entry, 0, @Model, 1.0, 1, 12340);
